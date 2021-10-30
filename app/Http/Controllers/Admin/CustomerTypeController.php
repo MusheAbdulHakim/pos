7601ebb592d95410027196e16a36fd94a401505f
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-30 17:22:47 +0000

    customer type controller

--- app/Http/Controllers/Admin/CustomerTypeController.php
+++ app/Http/Controllers/Admin/CustomerTypeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CustomerType;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class CustomerTypeController extends Controller
 {
@@ -12,20 +14,28 @@ class CustomerTypeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $title = 'customer type';
+        if ($request->ajax()){
+            $data = CustomerType::get();
+            return DataTables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action',function ($row){
+                        $editbtn = '<a data-id="'.$row->id.'" data-name="'.$row->name.'" href="javascript:void(0)" class="edit"><button class="btn btn-primary"><i class="fas fa-edit"></i></button></a>';
+                        $deletebtn = '<a data-id="'.$row->id.'" data-route="'.route('customer-type').'" href="javascript:void(0)" id="deletebtn"><button class="btn btn-danger"><i class="fas fa-trash"></i></button></a>';
+                        $btn = $editbtn.' '.$deletebtn;
+                        return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+        return view('admin.customertype.index',compact(
+            'title',
+        ));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    
 
     /**
      * Store a newly created resource in storage.
@@ -35,51 +45,45 @@ class CustomerTypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required|min:3|max:255',
+        ]);
+        CustomerType::create([
+            'name' => $request->name,
+        ]);
+        $notification = notify("customer type created successfully");
+        return back()->with($notification);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required|min:3|max:255',
+        ]);
+        CustomerType::findOrFail($request->id)->update([
+            'name' => $request->name,
+        ]);
+        $notification = notify("customer type updated successfully");
+        return back()->with($notification);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        return CustomerType::findOrFail($request->id)->delete();
     }
 }
