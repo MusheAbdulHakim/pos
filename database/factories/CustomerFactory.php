98adb97e4b427fbb54f9bdaa7f3a387e9a516ba4
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-30 17:12:43 +0000

    customer factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use App\Models\CustomerType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'customer_type_id' => CustomerType::get('id'),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
