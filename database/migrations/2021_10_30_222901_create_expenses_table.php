5bf2f803e16b0fd68b4da53330e440aec7b97c32
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-31 00:14:18 +0000

    expense migration

---
+++ database/migrations/2021_10_30_222901_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('expense_category_id')->nullable()->constrained()->onDelete('cascade');
+            $table->double('amount',8,2);
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
