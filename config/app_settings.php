7a1218361f69acf5e6354f38a44b686e956536ca
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-31 12:24:31 +0000

    updated

--- config/app_settings.php
+++ config/app_settings.php
@@ -7,7 +7,7 @@ return [
         'app' => [
             'title' => 'General Settings',
             'descriptions' => 'Application general settings.', // (optional)
-            'icon' => 'fa fa-cog', // (optional)
+            'icon' => 'fas fa-cog', // (optional)
 
             'inputs' => [
                 [
@@ -19,7 +19,7 @@ return [
                     'class' => 'form-control', // override global input_class
                     'style' => '', // any inline styles
                     'rules' => 'required|min:2|max:20', // validation rules for this input
-                    'value' => 'QCode', // any default value
+                    'value' => config('app.name'), // any default value
                     'hint' => 'You can set the app name here' // help block text for input
                 ],
                 [
@@ -33,29 +33,32 @@ return [
                     'preview_class' => 'thumbnail',
                     'preview_style' => 'height:40px'
                 ]
-            ]
-        ],
-        'email' => [
-            'title' => 'Email Settings',
-            'descriptions' => 'How app email will be sent.',
-            'icon' => 'fa fa-envelope',
-
-            'inputs' => [
+                , 
                 [
-                    'name' => 'from_email',
-                    'type' => 'email',
-                    'label' => 'From Email',
-                    'placeholder' => 'Application from email',
-                    'rules' => 'required|email',
+                    'name' => 'favicon',
+                    'type' => 'image',
+                    'label' => 'Upload favicon',
+                    'hint' => 'Recommended image size is 16px x 16px or 32px x 32px',
+                    'rules' => 'file|max:500',
+                    'disk' => 'public', // which disk you want to upload
+                    'path' => 'logos', // path on the disk,
+                    'preview_class' => 'thumbnail',
+                    'preview_style' => 'height:40px'
                 ],
                 [
-                    'name' => 'from_name',
+                    'name' => 'app_currency',
                     'type' => 'text',
-                    'label' => 'Email from Name',
-                    'placeholder' => 'Email from Name',
-                ]
+                    'label' => 'App Currency',
+                    'placeholder' => 'Application Currency',
+                    'class' => 'form-control',
+                    'style' => '', // any inline styles
+                    'rules' => 'required|max:10', // validation rules for this input
+                    'value' => '$', // any default value
+                    'hint' => 'Use your currency symbol like $',
+                ],
             ]
-        ]
+        ],
+        
     ],
 
     // Setting page url, will be used for get and post request
@@ -65,8 +68,8 @@ return [
     'middleware' => [],
 
     // View settings
-    'setting_page_view' => 'app_settings::settings_page',
-    'flash_partial' => 'app_settings::_flash',
+    'setting_page_view' => 'admin.settings.index',
+    'flash_partial' => '',
 
     // Setting section class setting
     'section_class' => 'card mb-3',
@@ -89,7 +92,7 @@ return [
     'remove_abandoned_settings' => false,
 
     // Controller to show and handle save setting
-    'controller' => '\QCod\AppSettings\Controllers\AppSettingController',
+    'controller' => '\App\Http\Controllers\Admin\SettingController',
 
     // settings group
     'setting_group' => function() {
