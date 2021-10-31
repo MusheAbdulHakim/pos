2d8cbec3a986597f72a9384985f1b83a39ab65b2
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-31 00:14:36 +0000

    expense factory

---
+++ database/factories/ExpenseFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Expense;
+use Illuminate\Support\Str;
+use App\Models\ExpenseCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExpenseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Expense::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'expense_category_id' => ExpenseCategory::get('id'),
+            'amount' => random_int(1,8),
+            'comment' => 'Hey this is a comment',
+        ];
+    }
+}
