a425c26fd99535caef2db83e81c4196c1d38d952
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-11-01 14:49:02 +0000

    unit table

---
+++ database/migrations/2021_10_31_200440_create_units_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->foreignId('unit_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('operator')->nullable();
+            $table->double('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
