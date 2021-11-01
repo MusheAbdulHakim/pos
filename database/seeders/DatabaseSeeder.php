f839bec4d961ec1382c3fde3d16e99dff57be8db
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-11-01 15:22:55 +0000

    resolve merge conflict

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
 use Database\Seeders\UserSeeder;
-=======
->>>>>>> e7e2e7a43cfaa6a7bd073ba9be0df634ee271e5a
 use Database\Seeders\RolePermissionSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,10 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             RolePermissionSeeder::class,
-<<<<<<< HEAD
             UserSeeder::class,
-=======
->>>>>>> e7e2e7a43cfaa6a7bd073ba9be0df634ee271e5a
         ]);
     }
 }
