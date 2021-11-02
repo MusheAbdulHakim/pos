52a7c78bef73f603dc1b22fc13d6f92fbf0e83a2
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-11-02 15:06:14 +0000

    placeholder added

--- resources/views/components/input/select2/multiple.blade.php
+++ resources/views/components/input/select2/multiple.blade.php
@@ -6,14 +6,13 @@
 
 <select name="{{$name}}" class="form-control select2" multiple="multiple" data-placeholder="Choose ..." id="{{$name}}">
     @foreach ($options as $option)
-        <option>{{$option->$key}}</option>
+        <option>{{$option->$key}}</option><option selected disabled>No Tax Method </option>
     @endforeach
 </select>
 
 
 @push('page-js')
 <script src="{{asset('assets/libs/select2/js/select2.min.js')}}"></script>
-<script src="{{asset('assets/js/pages/form-advanced.init.js')}}"></script>
 <script>
     $('.select2').select2();
 </script>
--- resources/views/components/input/select2/single.blade.php
+++ resources/views/components/input/select2/single.blade.php
@@ -4,7 +4,8 @@
 <link href="{{asset('assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
 @endpush
 
-<select name="{{$name}}" id="{{$name}}" class="form-control select2">
+<select name="{{$name}}" id="{{$name}}" class="form-control select2" >
+    <option value='' disable>Choose Option</option>
     @foreach ($options as $option)
         <option {{$option->$key == $selected ? 'selected': ''}} value="{{$option->id}}">{{$option->$key}}</option>
     @endforeach        
@@ -12,7 +13,6 @@
 
 @push('page-js')
 <script src="{{asset('assets/libs/select2/js/select2.min.js')}}"></script>
-<script src="{{asset('assets/js/pages/form-advanced.init.js')}}"></script>
 
 <script>
     $('.select2').select2();
