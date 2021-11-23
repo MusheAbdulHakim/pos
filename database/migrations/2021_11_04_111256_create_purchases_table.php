2e4fe9e17f7c95a01fb3b545dc7ff76a3822dbbd
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-11-23 23:39:52 +0000

    purchases table

---
+++ database/migrations/2021_11_04_111256_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference');
+            $table->foreignId('supplier_id')->nullable()->constrained()->onDelete('cascade');
+            $table->jsonb('products');
+            $table->string('status');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
